402f01a4fd116978b3e85d2e70471c9ce732b867
FadumaM <faduma.mohammed@gmail.com>
2016-08-02 14:55:02 +0100

    Completed looping of to-do list

--- build/js/app.js
+++ build/js/app.js
@@ -2,7 +2,14 @@ window.onload = function () {
   initialize();
 };
 
-var tasks = [];
+var tasks = [
+  'Wake up at 5:30 a.m',
+  'Consult my Tomorrow List',
+  'Head to the computer and start writing a blog post',
+  'Shower/breakfast at 7:00 a.m.',
+  'Back to writing',
+];
+var completed = [];
 
 function initialize() {
   bindEventListeners();
@@ -14,6 +21,14 @@ function bindEventListeners() {
   var addTaskPage   = document.getElementById('addTask');
   var completedPage = document.getElementById('completed');
   newTask.addEventListener('click', addTask);
+  toDoPage.addEventListener('click', goToDoPage);
+  addTaskPage.addEventListener('click', goAddTaskPage);
+  completedPage.addEventListener('click', goCompletedPage);
+
+}
+
+function goAddTaskPage() {
+  console.log('addTaskPage');
 }
 
 function addTask() {
@@ -21,5 +36,52 @@ function addTask() {
   var userInput = document.getElementById('userInput').value;
   tasks.push(userInput);
   document.getElementById('message').innerHTML = 'New task created!: ' + userInput;
-  console.log(tasks);
+}
+
+function goToDoPage() {
+  console.log('toDoPage');
+  showToDoList();
+}
+
+function showToDoList() {
+  var form = document.getElementById('form');
+  if (form.style.display == 'block') {
+    form.style.display = 'none';
+  }else {
+    form.style.display = 'block';
+  }
+
+  var list = document.createElement('ul');
+  var element = document.getElementById('div1');
+  element.appendChild(list);
+  list.className += 'lionheart-to-do-list';
+
+  for (var i = 0; i < tasks.length; i++) {
+    var item = document.createElement('li');
+    item.appendChild(document.createTextNode(tasks[i]));
+    list.appendChild(item);
+    item.className += 'lionheart-to-do';
+  }
+
+  var listOfTasks   = document.getElementsByClassName('lionheart-to-do');
+  for (i = 0; i < listOfTasks.length; i++) {
+    listOfTasks[i].addEventListener('click', completeTasks);
+  }
+}
+
+function goCompletedPage() {
+  var list = document.createElement('ul');
+  var element = document.getElementById('div1');
+  element.appendChild(list);
+  list.className += 'lionheart-completed-list';
+  for (var i = 0; i < completed.length; i++) {
+    var item = document.createElement('li');
+    item.appendChild(document.createTextNode(completed[i]));
+    list.appendChild(item);
+    item.className += 'lionheart-completed-to-do';
+  }
+}
+
+function completeTasks() {
+  completed.push(event.target.innerHTML);
 }
